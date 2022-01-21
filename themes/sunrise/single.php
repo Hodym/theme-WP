dca084aa622a3453f25fc23c7289dac9dab95e5e
Hodym <hodimchukdmitriy@gmail.com>
2022-01-21 22:15:27 +0200

    post entry output

--- themes/sunrise/single.php
+++ themes/sunrise/single.php
@@ -1,40 +1,19 @@
-<?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package sunrise
- */
+<?php get_header(); ?>
 
-get_header();
-?>
+<div class="site-content flex-grow-1 flex-shrink-0 mt-5 mt-md-0">
+	<section class="page mt-3">
+		<div class="container">
 
-	<main id="primary" class="site-main">
+			<?php while (have_posts()) : the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+				get_template_part('template-parts/content', get_post_type());
 
-			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'sunrise' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'sunrise' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			endwhile; ?>
 
-		endwhile; // End of the loop.
-		?>
+		</div>
+	</section>
+</div>
 
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
